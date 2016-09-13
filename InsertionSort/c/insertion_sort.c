8733e4c9eab8f7c88f07c1b7c6da026cb979fd7e
Jordan Dodson <jordandodson22@gmail.com>
2016-09-13 17:25:14 -0500

    replacing incorrect version of insertion sort

--- InsertionSort/c/insertion_sort.c
+++ InsertionSort/c/insertion_sort.c
@@ -1,19 +1,18 @@
 
-
 void insertion_sort(int* arr, int p, int r) {
 
   int i, j, t;
 
   for (i=p+1; i<r; i++) {
-    j = i;
     t = arr[i];
+    j = i-1;
 
-    while ((j>p) && (arr[j] < arr[j-1])) {
-      arr[j]=arr[j-1];
+    while ((j>=p) && (t < arr[j])) {
+      arr[j+1]=arr[j];
       j--;
     }
 
-    arr[j] = t;
+    arr[j+1] = t;
   }
-
 }
+
